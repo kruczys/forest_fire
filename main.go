b27369c6b892350a9b1884fd5217fb767d347854
Konrad Kreczko <konradkreczko@gmail.com>
2024-05-08 22:53:08 +0200

    oop :monkey:

--- main.go
+++ main.go
@@ -5,35 +5,55 @@ import (
 	"math/rand"
 )
 
-func createForest(treeProbability float32, forestSize int) (forest [][]int, err error) {
-	forest = make([][]int, forestSize)
+type forest struct {
+	dimensions int
+	trees      [][]int
+}
 
-	for i := range forest {
-		forest[i] = make([]int, forestSize)
-		for j := range forest[i] {
+func (f *forest) populateForest(treeProbability float32, forestSize int) {
+	f.dimensions = forestSize
+	f.trees = make([][]int, f.dimensions)
+
+	for i := range f.trees {
+		f.trees[i] = make([]int, f.dimensions)
+		for j := range f.trees[i] {
 			if rand.Float32() < treeProbability {
-				forest[i][j] = 1
+				f.trees[i][j] = 1
 			}
 		}
 	}
+}
 
+func lightningStrikeCoords(forestSize int) (x, y int) {
+	x = rand.Intn(forestSize)
+	y = rand.Intn(forestSize)
 	return
 }
 
-func lightningStrike(forestSize int) (x, y int) {
-	x = forestSize
-	return
+func (f *forest) didLigthningHitTree(x, y int) bool {
+	return f.trees[x][y] == 1
 }
 
-func printForest(forest [][]int) {
-	for i := 0; i < len(forest); i++ {
-		fmt.Println(forest[i])
+func (f *forest) printForest() {
+	for i := 0; i < len(f.trees); i++ {
+		for j := 0; j < len(f.trees[i]); j++ {
+			switch f.trees[i][j] {
+			case 1:
+				fmt.Print("\033[32m4\033[0m ")
+			case 0:
+				fmt.Print("\033[30m0\033[0m ")
+			case -1:
+				fmt.Print("\033[31m-4\033[0m ")
+			default:
+				fmt.Print(f.trees[i][j], " ")
+			}
+		}
+		fmt.Println()
 	}
 }
 
 func main() {
-	forest, err := createForest(0.3, 5)
-	if err == nil {
-		printForest(forest)
-	}
+	var forest forest
+	forest.populateForest(0.3, 10)
+	forest.printForest()
 }
